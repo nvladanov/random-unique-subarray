a1ccac9fa4dd61558f546a082d0960cbdfe6b00d
ladanov <ladanov_n@mail.ru>
2019-08-18 19:34:14 +0300

    added package.json description

---
+++ src/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const shuffle = require("array-shuffle");
+const removeDuplicates = require("removeduplicates").default;
+
+/**
+ * Returns a random subarray of the specified length with unique elements
+ * @param {Array} array
+ * @param {Number} newArrLength
+ */
+const uniqueSubarr = (array, newArrLength, options = {
+    removeDuplicates: false,
+    key: undefined
+}) => {
+
+  const uniqueArr = options.removeDuplicates 
+    ? removeDuplicates(array, options.key) 
+    : array;
+    
+  if (newArrLength > uniqueArr.length)
+    throw new Error(`Subarray can't be larger than an array`);
+
+  const shuffledArr = shuffle(uniqueArr);
+  return shuffledArr.slice(0, newArrLength);
+};
+
+module.exports = uniqueSubarr;
